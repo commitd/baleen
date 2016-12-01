13f4494b3513024d60c86f7df1426c08b9865539
chrisflatley <chrisflatley@tenode.com>
2016-12-01 13:24:32 +0000

    Clarify StructureContentExtractor code

--- baleen/baleen-collectionreaders/src/main/java/uk/gov/dstl/baleen/contentextractors/StructureContentExtractor.java
+++ baleen/baleen-collectionreaders/src/main/java/uk/gov/dstl/baleen/contentextractors/StructureContentExtractor.java
@@ -1,4 +1,4 @@
-//Dstl (c) Crown Copyright 2015
+// Dstl (c) Crown Copyright 2015
 package uk.gov.dstl.baleen.contentextractors;
 
 import java.io.IOException;
@@ -6,12 +6,14 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.apache.tika.metadata.Metadata;
 import org.apache.uima.UIMAException;
 import org.apache.uima.UimaContext;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.CasCopier;
 import org.slf4j.Logger;
@@ -24,7 +26,7 @@ import com.tenode.baleen.extraction.tika.TikaFormatExtractor;
 
 import uk.gov.dstl.baleen.contentextractors.helpers.AbstractContentExtractor;
 import uk.gov.dstl.baleen.contentextractors.helpers.SimpleTagToStructureMapper;
-import uk.gov.dstl.baleen.contentextractors.processor.IContentProcessor;
+import uk.gov.dstl.baleen.contentextractors.processor.ContentProcessor;
 import uk.gov.dstl.baleen.cpe.CpeBuilderUtils;
 import uk.gov.dstl.baleen.exceptions.InvalidParameterException;
 
@@ -41,32 +43,33 @@ public class StructureContentExtractor extends AbstractContentExtractor {
 	/** The Constant CONTENT_EXTRATOR_DEFAULT_PACKAGE. */
 	public static final String CONTENT_PROCESSOR_DEFAULT_PACKAGE = "uk.gov.dstl.baleen.contentextractors.processor";
 
-	private List<IContentProcessor> processors;
+	private List<ContentProcessor> processors;
 
 	@Override
 	public void doInitialize(UimaContext context, Map<String, Object> params) throws ResourceInitializationException {
 		super.doInitialize(context, params);
 
-		Object config = params.get("contentProcessors");
+		final Object config = params.get("contentProcessors");
 		if (config != null && config instanceof String[]) {
 			try {
 				processors = createContentProcessors(context, (String[]) config);
-			} catch (InvalidParameterException e) {
-			  throw new ResourceInitializationException(e);
+			} catch (final InvalidParameterException e) {
+				throw new ResourceInitializationException(e);
 			}
 		}
 
 	}
 
-	private List<IContentProcessor> createContentProcessors(UimaContext context, String[] contentProcesors)
+	private List<ContentProcessor> createContentProcessors(UimaContext context, String[] contentProcesors)
 			throws InvalidParameterException {
-		List<IContentProcessor> newProcessors = new ArrayList<>();
-		for (String contentProcesor : contentProcesors) {
+		final List<ContentProcessor> newProcessors = new ArrayList<>();
+		for (final String contentProcesor : contentProcesors) {
 			try {
-				IContentProcessor processor = (IContentProcessor) CpeBuilderUtils
+				final ContentProcessor processor = (ContentProcessor) CpeBuilderUtils
 						.getClassFromString(contentProcesor, CONTENT_PROCESSOR_DEFAULT_PACKAGE).newInstance();
 
-				processor.initialize(context, getSupport(), getMonitor()); // Consider params
+				processor.initialize(context, getSupport(), getMonitor()); // Consider
+																			// params
 				newProcessors.add(processor);
 			} catch (InstantiationException | IllegalAccessException | ResourceInitializationException e) {
 				LOGGER.info("Could not find or instantiate content processor " + contentProcesor, e);
@@ -79,36 +82,50 @@ public class StructureContentExtractor extends AbstractContentExtractor {
 	public void doProcessStream(InputStream stream, String source, JCas jCas) throws IOException {
 
 		try {
-			TikaFormatExtractor formatExtractor = new TikaFormatExtractor();
+			final TikaFormatExtractor formatExtractor = new TikaFormatExtractor();
 
-			TikaExtraction extraction = formatExtractor.parse(stream, source);
+			final TikaExtraction extraction = formatExtractor.parse(stream, source);
 
-			JCas input = JCasFactory.createJCas();
-			input.setDocumentText(extraction.getText());
+			final JCas structuredJCas = JCasFactory.createJCas();
+			structuredJCas.setDocumentText(extraction.getText());
 
-			SimpleTagToStructureMapper mapper = new SimpleTagToStructureMapper(input, extraction.getMetadata());
-			extraction.getTags().stream().map(mapper::map).forEach(s -> s.ifPresent(this::addToJCasIndex));
+			final SimpleTagToStructureMapper mapper = new SimpleTagToStructureMapper(structuredJCas,
+					extraction.getMetadata());
+			extraction.getTags().stream().map(mapper::map).filter(Optional::isPresent).map(Optional::get)
+					.forEach(Annotation::addToIndexes);
 
-			JCas output = JCasFactory.createJCas();
-			for (IContentProcessor processor : processors) {
-				processor.process(input, output);
-				JCas tmp = input;
-				tmp.reset();
-				input = output;
-				output = tmp;
+			JCas manipulatedJCas;
+			if (processors == null || processors.isEmpty()) {
+				manipulatedJCas = structuredJCas;
+			} else {
+				JCas input = structuredJCas;
+				JCas output = JCasFactory.createJCas();
+				for (final ContentProcessor processor : processors) {
+					output.reset();
+					processor.process(input, output);
+					final JCas tmp = input;
+					input = output;
+					output = tmp;
+				}
+				manipulatedJCas = input;
 			}
 
-			CasCopier.copyCas(input.getCas(), jCas.getCas(), true);
-	        super.doProcessStream(stream, source, jCas);
+			// JCasUtil.select(manipulatedJCas, Heading.class).stream()
+			// .forEach(h -> System.out.println(h.getLevel()));
+			CasCopier.copyCas(manipulatedJCas.getCas(), jCas.getCas(), true);
+			// JCasUtil.select(jCas, Heading.class).stream().forEach(h ->
+			// System.out.println(h.getLevel()));
 
-			Metadata metadata = extraction.getMetadata();
-			for (String name : metadata.names()) {
+			super.doProcessStream(stream, source, jCas);
+
+			final Metadata metadata = extraction.getMetadata();
+			for (final String name : metadata.names()) {
 				addMetadata(jCas, name, metadata.get(name));
 			}
-		} catch (UIMAException e) {
+		} catch (final UIMAException e) {
 			getMonitor().warn("Couldn't process document from '{}'", source, e);
 			setCorrupt(jCas);
-		} catch (ExtractionException e) {
+		} catch (final ExtractionException e) {
 			getMonitor().warn("Couldn't extract structure from document '{}'", source, e);
 			setCorrupt(jCas);
 		}
