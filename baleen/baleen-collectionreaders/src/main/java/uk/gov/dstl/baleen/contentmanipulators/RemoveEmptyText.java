ac379a9c442ff00d5341aa642b0768f3d1b46595
Chris Flatley <chrisflatley@tenode.com>
2016-12-22 21:15:16 +0100

    Recursively remove empty tags in content manipulator

--- baleen/baleen-collectionreaders/src/main/java/uk/gov/dstl/baleen/contentmanipulators/RemoveEmptyText.java
+++ baleen/baleen-collectionreaders/src/main/java/uk/gov/dstl/baleen/contentmanipulators/RemoveEmptyText.java
@@ -1,6 +1,7 @@
 package uk.gov.dstl.baleen.contentmanipulators;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import uk.gov.dstl.baleen.contentmanipulators.helpers.ContentManipulator;
 
@@ -8,7 +9,18 @@ public class RemoveEmptyText implements ContentManipulator {
 
   @Override
   public void manipulate(final Document document) {
-    document.select(":empty").remove();
+    while (!removeEmpty(document)) {
+      // Repeat as needed.... work done in the while
+    }
+  }
+
+  private boolean removeEmpty(final Document document) {
+    final Elements emptyNodes = document.select(":empty");
+    if (emptyNodes.isEmpty()) {
+      return true;
+    }
+    emptyNodes.remove();
+    return false;
   }
 
 }
