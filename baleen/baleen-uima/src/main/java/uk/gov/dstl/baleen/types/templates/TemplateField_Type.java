fb9a359dde2d76a33c3966cb8d481dd6b2987a74
James Fry <james.fry@jrfsolutions.co.uk>
2017-02-02 12:16:10 +0000

    Adds template field annotation to type system

---
+++ baleen/baleen-uima/src/main/java/uk/gov/dstl/baleen/types/templates/TemplateField_Type.java
@@ -0,0 +1,69 @@
+
+/* First created by JCasGen Thu Feb 02 11:58:46 GMT 2017 */
+package uk.gov.dstl.baleen.types.templates;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import uk.gov.dstl.baleen.types.Base_Type;
+
+/** An field identified from a template
+ * Updated by JCasGen Thu Feb 02 11:58:46 GMT 2017
+ * @generated */
+public class TemplateField_Type extends Base_Type {
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = TemplateField.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("uk.gov.dstl.baleen.types.templates.TemplateField");
+ 
+  /** @generated */
+  final Feature casFeat_name;
+  /** @generated */
+  final int     casFeatCode_name;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getName(int addr) {
+        if (featOkTst && casFeat_name == null)
+      jcas.throwFeatMissing("name", "uk.gov.dstl.baleen.types.templates.TemplateField");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_name);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setName(int addr, String v) {
+        if (featOkTst && casFeat_name == null)
+      jcas.throwFeatMissing("name", "uk.gov.dstl.baleen.types.templates.TemplateField");
+    ll_cas.ll_setStringValue(addr, casFeatCode_name, v);}
+    
+  
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * @generated
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public TemplateField_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_name = jcas.getRequiredFeatureDE(casType, "name", "uima.cas.String", featOkTst);
+    casFeatCode_name  = (null == casFeat_name) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_name).getCode();
+
+  }
+}
+
+
+
+    
