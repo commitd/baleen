da13209d46e598e80a2e42c05e5f76c51c2863fa
James Fry <james.fry@jrfsolutions.co.uk>
2017-02-15 17:31:00 +0000

    Recursive JSON output for FeatureStructures (and reformat code)

--- baleen/baleen-consumers/src/main/java/uk/gov/dstl/baleen/consumers/json/AbstractJsonConsumer.java
+++ baleen/baleen-consumers/src/main/java/uk/gov/dstl/baleen/consumers/json/AbstractJsonConsumer.java
@@ -1,123 +1,179 @@
 package uk.gov.dstl.baleen.consumers.json;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.uima.UimaContext;
-import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.Feature;
-import org.apache.uima.fit.descriptor.ConfigurationParameter;
-import org.apache.uima.jcas.JCas;
-import org.apache.uima.resource.ResourceInitializationException;
-import uk.gov.dstl.baleen.consumers.utils.SourceUtils;
-import uk.gov.dstl.baleen.types.BaleenAnnotation;
-import uk.gov.dstl.baleen.uima.BaleenConsumer;
-
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSArray;
+import org.apache.uima.jcas.cas.Sofa;
+import org.apache.uima.resource.ResourceInitializationException;
+import uk.gov.dstl.baleen.consumers.utils.SourceUtils;
+import uk.gov.dstl.baleen.types.BaleenAnnotation;
+import uk.gov.dstl.baleen.uima.BaleenConsumer;
 
 public abstract class AbstractJsonConsumer extends BaleenConsumer {
 
-  public static final String PARAM_OUTPUT_FILE = "outputDirectory";
-  @ConfigurationParameter(name = PARAM_OUTPUT_FILE, defaultValue = "jsonOutput")
-  private String outputDirectory = "jsonOutput";
+	public static final String PARAM_OUTPUT_FILE = "outputDirectory";
+	@ConfigurationParameter(name = PARAM_OUTPUT_FILE, defaultValue = "jsonOutput")
+	private String outputDirectory = "jsonOutput";
 
-  private final ObjectMapper objectMapper;
+	private final ObjectMapper objectMapper;
 
-  public AbstractJsonConsumer() {
-    objectMapper = new ObjectMapper();
-    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-  }
+	public AbstractJsonConsumer() {
+		objectMapper = new ObjectMapper();
+		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+	}
 
-  @Override
-  public void doInitialize(final UimaContext aContext) throws ResourceInitializationException {
-    super.doInitialize(aContext);
-    getMonitor()
-        .info("Will be writing to " + Paths.get(outputDirectory).toAbsolutePath().toString());
-  }
+	@Override
+	public void doInitialize(final UimaContext aContext) throws ResourceInitializationException {
+		super.doInitialize(aContext);
+		getMonitor().info("Will be writing to " + Paths.get(outputDirectory).toAbsolutePath().toString());
+	}
 
-  @Override
-  protected void doProcess(JCas jCas) throws AnalysisEngineProcessException {
-    String documentSourceName = SourceUtils.getDocumentSourceBaseName(jCas, getSupport());
+	@Override
+	protected void doProcess(JCas jCas) throws AnalysisEngineProcessException {
+		String documentSourceName = SourceUtils.getDocumentSourceBaseName(jCas, getSupport());
 
-    try (Writer writer = createOutputWriter(documentSourceName)) {
-      writeJson(writer, selectAnnotations(jCas));
-    } catch (IOException e) {
-      getMonitor().warn("Failed to write JSON for " + documentSourceName, e);
-    }
-  }
+		JsonFactory factory = objectMapper.getFactory();
+		try (Writer writer = createOutputWriter(documentSourceName);
+				JsonGenerator generator = factory.createGenerator(writer).useDefaultPrettyPrinter()) {
+			generator.writeStartObject();
+			writeSofa(generator, jCas);
+			writeAnnotations(generator, selectAnnotations(jCas));
+			generator.writeEndObject();
+		} catch (IOException e) {
+			getMonitor().warn("Failed to write JSON for " + documentSourceName, e);
+		}
+	}
 
-  private Writer createOutputWriter(final String documentSourceName) throws IOException {
-    Path directoryPath = Paths.get(outputDirectory);
-    if (!Files.exists(directoryPath)) {
-      Files.createDirectories(directoryPath);
-    }
-    String baseName = FilenameUtils.getBaseName(documentSourceName);
-    Path outputFilePath = directoryPath.resolve(baseName + ".properties");
-    if (Files.exists(outputFilePath)) {
-      getMonitor().warn("Overwriting existing output file {}", outputFilePath.toString());
-    }
-    return Files.newBufferedWriter(outputFilePath, StandardCharsets.UTF_8);
-  }
+	private void writeSofa(JsonGenerator generator, JCas jCas) throws IOException {
+		Sofa sofa = jCas.getSofa();
+		generator.writeFieldName("sofa");
+		writeJson(generator, sofa);
+	}
 
-  private void writeJson(Writer writer, Iterable<? extends BaleenAnnotation> selectedAnnotations)
-      throws IOException {
-    List<JsonAnnotation> jsonAnnotations = new ArrayList<>();
-    for (BaleenAnnotation annotation : selectedAnnotations) {
-      jsonAnnotations.add(new JsonAnnotation(annotation));
-    }
-    objectMapper.writeValue(writer, jsonAnnotations);
-  }
+	private Writer createOutputWriter(final String documentSourceName) throws IOException {
+		Path directoryPath = Paths.get(outputDirectory);
+		if (!Files.exists(directoryPath)) {
+			Files.createDirectories(directoryPath);
+		}
+		String baseName = FilenameUtils.getBaseName(documentSourceName);
+		Path outputFilePath = directoryPath.resolve(baseName + ".json");
+		if (Files.exists(outputFilePath)) {
+			getMonitor().warn("Overwriting existing output file {}", outputFilePath.toString());
+		}
+		return Files.newBufferedWriter(outputFilePath, StandardCharsets.UTF_8);
+	}
 
-  protected abstract Iterable<? extends BaleenAnnotation> selectAnnotations(JCas jCas);
+	private void writeAnnotations(JsonGenerator generator, Iterable<? extends BaleenAnnotation> selectedAnnotations)
+			throws IOException {
+		generator.writeFieldName("annotations");
+		generator.writeStartArray();
+		for (BaleenAnnotation baleenAnnotation : selectedAnnotations) {
+			writeJson(generator, baleenAnnotation);
+		}
+		generator.writeEndArray();
+	}
 
-  static final class JsonAnnotation {
-    private int begin;
-    private int end;
-    private String type;
-    private Map<String, String> attributes;
+	private void writeJson(JsonGenerator generator, FeatureStructure annotation) throws IOException {
+		generator.writeStartObject();
+		Type type = annotation.getType();
+		List<Feature> features = type.getFeatures();
+		if (annotation instanceof AnnotationFS) {
+			AnnotationFS annotationFS = (AnnotationFS) annotation;
+			generator.writeStringField("coveredText", annotationFS.getCoveredText());
+		}
+		for (Feature feature : features) {
+			if (feature.getRange().isPrimitive()) {
+				writePrimitive(generator, annotation, feature);
+			} else if (feature.getRange().isArray()) {
+				writeArray(generator, annotation, feature);
+			} else {
+				FeatureStructure featureValue = annotation.getFeatureValue(feature);
+				if (featureValue != null) {
+					if (!"uima.cas.AnnotationBase:sofa".equals(feature.getName())) {
+						generator.writeFieldName(feature.getShortName());
+						writeJson(generator, featureValue);
+					}
+				}
+			}
+		}
+		generator.writeEndObject();
+	}
 
-    public JsonAnnotation(BaleenAnnotation annotation) {
-      this.begin = annotation.getBegin();
-      this.end = annotation.getEnd();
-      this.type = annotation.getType().getName();
-      this.attributes = makeAttributes(annotation);
-    }
+	private void writeArray(JsonGenerator generator, FeatureStructure annotation, Feature feature) throws IOException {
+		if (annotation instanceof FSArray) {
+			FSArray fsArray = (FSArray) annotation;
+			generator.writeFieldName(feature.getShortName());
+			FeatureStructure[] array = fsArray.toArray();
+			if (array.length > 0) {
+				generator.writeStartArray();
+				for (FeatureStructure featureStructure : array) {
+					writePrimitiveValue(generator, featureStructure, feature);
+				}
+			}
+			generator.writeEndArray();
+		}
+	}
 
-    private Map<String, String> makeAttributes(BaleenAnnotation annotation) {
-      Map<String, String> attributeValues = new HashMap<>();
-      List<Feature> features = annotation.getType().getFeatures();
-      for (Feature feature : features) {
-        // for now coerce all feature values to strings, ignore non-primitives
-        if (feature.getRange().isPrimitive()) {
-          attributeValues.put(feature.getName(), annotation.getFeatureValueAsString(feature));
-        }
-      }
-      return attributeValues;
-    }
+	private void writePrimitive(JsonGenerator generator, FeatureStructure annotation, Feature feature)
+			throws IOException {
+		generator.writeFieldName(feature.getShortName());
+		writePrimitiveValue(generator, annotation, feature);
+	}
 
-    public int getBegin() {
-      return begin;
-    }
+	private void writePrimitiveValue(JsonGenerator generator, FeatureStructure annotation, Feature feature)
+			throws IOException {
+		String range = feature.getRange().getName();
+		switch (range) {
+		case CAS.TYPE_NAME_INTEGER:
+			generator.writeNumber(annotation.getIntValue(feature));
+			break;
+		case CAS.TYPE_NAME_FLOAT:
+			generator.writeNumber(annotation.getFloatValue(feature));
+			break;
+		case CAS.TYPE_NAME_STRING:
+			generator.writeString(annotation.getStringValue(feature));
+			break;
+		case CAS.TYPE_NAME_BOOLEAN:
+			generator.writeBoolean(annotation.getBooleanValue(feature));
+			break;
+		case CAS.TYPE_NAME_BYTE:
+			generator.writeNumber(annotation.getByteValue(feature));
+			break;
+		case CAS.TYPE_NAME_SHORT:
+			generator.writeNumber(annotation.getShortValue(feature));
+			break;
+		case CAS.TYPE_NAME_LONG:
+			generator.writeNumber(annotation.getLongValue(feature));
+			break;
+		case CAS.TYPE_NAME_DOUBLE:
+			generator.writeNumber(annotation.getDoubleValue(feature));
+			break;
+		default:
+			getMonitor().warn("Unexpected primitive type: " + range);
+			break;
+		}
+	}
 
-    public int getEnd() {
-      return end;
-    }
+	protected abstract Iterable<? extends BaleenAnnotation> selectAnnotations(JCas jCas);
 
-    public String getType() {
-      return type;
-    }
-
-    public Map<String, String> getAttributes() {
-      return attributes;
-    }
-  }
 }
