ce17bd05f015f4dac692cd7b4151c247fa6f1bf1
James Fry <james.fry@jrfsolutions.co.uk>
2017-03-30 01:17:44 +0100

    Refactor Mustache consumer classes for better reuse

--- baleen/baleen-consumers/src/main/java/uk/gov/dstl/baleen/consumers/template/AbstractMustacheHtmlTemplateRecordConsumer.java
+++ baleen/baleen-consumers/src/main/java/uk/gov/dstl/baleen/consumers/template/AbstractMustacheHtmlTemplateRecordConsumer.java
@@ -9,17 +9,17 @@ import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.samskivert.mustache.Mustache;
 import com.samskivert.mustache.Mustache.Compiler;
 import com.samskivert.mustache.Template;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.resource.ResourceInitializationException;
+import uk.gov.dstl.baleen.consumers.template.ExtractedRecord.Kind;
 import uk.gov.dstl.baleen.consumers.utils.SingleDocumentConsumerFormat;
 import uk.gov.dstl.baleen.types.metadata.Metadata;
 
@@ -30,17 +30,6 @@ import uk.gov.dstl.baleen.types.metadata.Metadata;
  * to template fields.
  */
 public abstract class AbstractMustacheHtmlTemplateRecordConsumer extends AbstractRecordConsumer {
-
-	/** The Constant PARAM_FILENAME. */
-	public static final String PARAM_FILENAME = "templateFilename";
-
-	/**
-	 * The template filename to use.
-	 *
-	 * @baleen.config template.html
-	 */
-	@ConfigurationParameter(name = PARAM_FILENAME, defaultValue = "template.html")
-	private String templateFilename;
 
 	/** The Constant PARAM_OUTPUT_DIRECTORY. */
 	public static final String PARAM_OUTPUT_DIRECTORY = "outputDirectory";
@@ -53,29 +42,18 @@ public abstract class AbstractMustacheHtmlTemplateRecordConsumer extends Abstrac
 	@ConfigurationParameter(name = PARAM_OUTPUT_DIRECTORY, defaultValue = "generatedDocuments")
 	private String outputDirectory = "generatedDocuments";
 
-	/** The template. */
-	private Template template;
-
-	@Override
-	public void initialize(UimaContext context) throws ResourceInitializationException {
-		super.initialize(context);
-		try {
-			compileTemplate();
-		} catch (IOException e) {
-			throw new ResourceInitializationException(e);
-		}
-	}
-
 	/**
 	 * Compile template.
+	 * 
+	 * @return
 	 *
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
 	 */
-	private void compileTemplate() throws IOException {
+	protected static Template compileTemplate(String templateFilename) throws IOException {
 		Compiler compiler = Mustache.compiler();
 		String templateHtml = new String(Files.readAllBytes(Paths.get(templateFilename)), StandardCharsets.UTF_8);
-		template = compiler.compile(templateHtml);
+		return compiler.compile(templateHtml);
 	}
 
 	@Override
@@ -89,12 +67,11 @@ public abstract class AbstractMustacheHtmlTemplateRecordConsumer extends Abstrac
 		fieldMap.put("metadata", metadataMap);
 		fieldMap.put("content", jCas.getDocumentText());
 
-		try (Writer writer = createOutputWriter(documentSourceName)) {
-			template.execute(fieldMap, writer);
-		} catch (IOException e) {
-			getMonitor().warn("Failed to process template " + templateFilename + " for " + documentSourceName, e);
-		}
+		writeRecords(documentSourceName, records, fieldMap);
 	}
+
+	protected abstract void writeRecords(String documentSourceName, Map<String, Collection<ExtractedRecord>> records,
+			Map<String, Object> fieldMap);
 
 	/**
 	 * Map records to a moustache field name and value.
@@ -110,7 +87,76 @@ public abstract class AbstractMustacheHtmlTemplateRecordConsumer extends Abstrac
 	 *            the records
 	 * @return the map of field name to value
 	 */
-	protected abstract Map<String, ?> mapFields(JCas jCas, Map<String, Collection<ExtractedRecord>> records);
+	private Map<String, ?> mapFields(JCas jCas, Map<String, Collection<ExtractedRecord>> records) {
+		Map<String, Object> context = new HashMap<>();
+
+		Map<String, String> flattenedFields = getFlattenedFields(records);
+		context.put("fields", flattenedFields);
+
+		Map<String, Map<String, String>> flattenedRecords = getFlattenedRecords(records);
+		context.put("records", flattenedRecords);
+
+		Map<String, Map<String, Map<String, String>>> sourceRecords = getSourceRecords(records);
+		context.put("sources", sourceRecords);
+
+		return context;
+	}
+
+	private static Map<String, String> getFlattenedFields(Map<String, Collection<ExtractedRecord>> records) {
+		Map<String, String> fieldMap = new HashMap<>();
+		for (Entry<String, Collection<ExtractedRecord>> entry : records.entrySet()) {
+			Collection<ExtractedRecord> sourceRecords = entry.getValue();
+			for (ExtractedRecord extractedRecord : sourceRecords) {
+				Collection<ExtractedField> fields = extractedRecord.getFields();
+				fields.forEach(field -> fieldMap.put(field.getName(), field.getValue()));
+			}
+		}
+		return fieldMap;
+	}
+
+	private static Map<String, Map<String, String>> getFlattenedRecords(
+			Map<String, Collection<ExtractedRecord>> records) {
+		Map<String, Map<String, String>> recordMap = new HashMap<>();
+		for (Entry<String, Collection<ExtractedRecord>> entry : records.entrySet()) {
+			Collection<ExtractedRecord> sourceRecords = entry.getValue();
+			for (ExtractedRecord extractedRecord : sourceRecords) {
+				if (extractedRecord.getKind() == Kind.DEFAULT) {
+					continue;
+				}
+				Collection<ExtractedField> fields = extractedRecord.getFields();
+				Map<String, String> fieldMap = new HashMap<>();
+				String name = extractedRecord.getName();
+				fields.forEach(field -> fieldMap.put(field.getName(), field.getValue()));
+				if (fieldMap.size() > 0) {
+					recordMap.put(name, fieldMap);
+				}
+			}
+		}
+		return recordMap;
+	}
+
+	private static Map<String, Map<String, Map<String, String>>> getSourceRecords(
+			Map<String, Collection<ExtractedRecord>> records) {
+		Map<String, Map<String, Map<String, String>>> sourceMap = new HashMap<>();
+		for (Entry<String, Collection<ExtractedRecord>> entry : records.entrySet()) {
+			String sourceName = entry.getKey();
+			Map<String, Map<String, String>> recordsMap = new HashMap<>();
+			Collection<ExtractedRecord> sourceRecords = entry.getValue();
+			for (ExtractedRecord extractedRecord : sourceRecords) {
+				if (extractedRecord.getKind() == Kind.DEFAULT) {
+					continue;
+				}
+				Collection<ExtractedField> fields = extractedRecord.getFields();
+				Map<String, String> recordFields = new HashMap<>();
+				recordsMap.put(extractedRecord.getName(), recordFields);
+				fields.forEach(field -> recordFields.put(field.getName(), field.getValue()));
+			}
+			if (recordsMap.size() > 0) {
+				sourceMap.put(sourceName, recordsMap);
+			}
+		}
+		return sourceMap;
+	}
 
 	/**
 	 * Creates an output writer for a new file in the configured output
@@ -125,7 +171,7 @@ public abstract class AbstractMustacheHtmlTemplateRecordConsumer extends Abstrac
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
 	 */
-	private Writer createOutputWriter(final String documentSourceName) throws IOException {
+	protected Writer createOutputWriter(final String documentSourceName) throws IOException {
 		Path directoryPath = Paths.get(outputDirectory);
 		if (!directoryPath.toFile().exists()) {
 			Files.createDirectories(directoryPath);
--- baleen/baleen-consumers/src/main/java/uk/gov/dstl/baleen/consumers/template/DefaultMustacheHtmlTemplateRecordConsumer.java
+++ baleen/baleen-consumers/src/main/java/uk/gov/dstl/baleen/consumers/template/DefaultMustacheHtmlTemplateRecordConsumer.java
@@ -1,85 +1,49 @@
 package uk.gov.dstl.baleen.consumers.template;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
-import org.apache.uima.jcas.JCas;
-import uk.gov.dstl.baleen.consumers.template.ExtractedRecord.Kind;
+import com.samskivert.mustache.Template;
+import org.apache.uima.UimaContext;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.resource.ResourceInitializationException;
 
 public class DefaultMustacheHtmlTemplateRecordConsumer extends AbstractMustacheHtmlTemplateRecordConsumer {
 
+	/** The Constant PARAM_FILENAME. */
+	public static final String PARAM_FILENAME = "templateFilename";
+
+	/**
+	 * The template filename to use.
+	 *
+	 * @baleen.config template.html
+	 */
+	@ConfigurationParameter(name = PARAM_FILENAME, defaultValue = "template.html")
+	private String templateFilename;
+
+	/** The template. */
+	private Template template;
+
 	@Override
-	protected Map<String, ?> mapFields(JCas jCas, Map<String, Collection<ExtractedRecord>> records) {
-		Map<String, Object> context = new HashMap<>();
-
-		Map<String, String> flattenedFields = getFlattenedFields(records);
-		context.put("fields", flattenedFields);
-
-		Map<String, Map<String, String>> flattenedRecords = getFlattenedRecords(records);
-		context.put("records", flattenedRecords);
-
-		Map<String, Map<String, Map<String, String>>> sourceRecords = getSourceRecords(records);
-		context.put("sources", sourceRecords);
-
-		return context;
+	public void initialize(UimaContext context) throws ResourceInitializationException {
+		super.initialize(context);
+		try {
+			template = compileTemplate(templateFilename);
+		} catch (IOException e) {
+			throw new ResourceInitializationException(e);
+		}
 	}
 
-	private static Map<String, String> getFlattenedFields(Map<String, Collection<ExtractedRecord>> records) {
-		Map<String, String> fieldMap = new HashMap<>();
-		for (Entry<String, Collection<ExtractedRecord>> entry : records.entrySet()) {
-			Collection<ExtractedRecord> sourceRecords = entry.getValue();
-			for (ExtractedRecord extractedRecord : sourceRecords) {
-				Collection<ExtractedField> fields = extractedRecord.getFields();
-				fields.forEach(field -> fieldMap.put(field.getName(), field.getValue()));
-			}
+	@Override
+	protected void writeRecords(String documentSourceName, Map<String, Collection<ExtractedRecord>> records,
+			Map<String, Object> fieldMap) {
+		try (Writer writer = createOutputWriter(documentSourceName)) {
+			template.execute(fieldMap, writer);
+		} catch (IOException e) {
+			getMonitor().warn("Failed to process template " + templateFilename + " for " + documentSourceName, e);
 		}
-		return fieldMap;
-	}
-
-	private static Map<String, Map<String, String>> getFlattenedRecords(
-			Map<String, Collection<ExtractedRecord>> records) {
-		Map<String, Map<String, String>> recordMap = new HashMap<>();
-		for (Entry<String, Collection<ExtractedRecord>> entry : records.entrySet()) {
-			Collection<ExtractedRecord> sourceRecords = entry.getValue();
-			for (ExtractedRecord extractedRecord : sourceRecords) {
-				if (extractedRecord.getKind() == Kind.DEFAULT) {
-					continue;
-				}
-				Collection<ExtractedField> fields = extractedRecord.getFields();
-				Map<String, String> fieldMap = new HashMap<>();
-				String name = extractedRecord.getName();
-				fields.forEach(field -> fieldMap.put(field.getName(), field.getValue()));
-				if (fieldMap.size() > 0) {
-					recordMap.put(name, fieldMap);
-				}
-			}
-		}
-		return recordMap;
-	}
-
-	private static Map<String, Map<String, Map<String, String>>> getSourceRecords(
-			Map<String, Collection<ExtractedRecord>> records) {
-		Map<String, Map<String, Map<String, String>>> sourceMap = new HashMap<>();
-		for (Entry<String, Collection<ExtractedRecord>> entry : records.entrySet()) {
-			String sourceName = entry.getKey();
-			Map<String, Map<String, String>> recordsMap = new HashMap<>();
-			Collection<ExtractedRecord> sourceRecords = entry.getValue();
-			for (ExtractedRecord extractedRecord : sourceRecords) {
-				if (extractedRecord.getKind() == Kind.DEFAULT) {
-					continue;
-				}
-				Collection<ExtractedField> fields = extractedRecord.getFields();
-				Map<String, String> recordFields = new HashMap<>();
-				recordsMap.put(extractedRecord.getName(), recordFields);
-				fields.forEach(field -> recordFields.put(field.getName(), field.getValue()));
-			}
-			if (recordsMap.size() > 0) {
-				sourceMap.put(sourceName, recordsMap);
-			}
-		}
-		return sourceMap;
 	}
 
 }
