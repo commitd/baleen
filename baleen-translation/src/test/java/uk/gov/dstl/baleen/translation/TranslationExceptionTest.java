d45e8749598de8f8c5ba7d85248bedb5ac65409b
Stuart Hendren <stuart@committed.io>
2018-08-22 16:00:26 +0100

    Add TranslationException

---
+++ baleen-translation/src/test/java/uk/gov/dstl/baleen/translation/TranslationExceptionTest.java
@@ -0,0 +1,27 @@
+package uk.gov.dstl.baleen.translation;
+
+import org.junit.Test;
+
+public class TranslationExceptionTest {
+
+
+  @Test(expected = TranslationException.class)
+  public void canConstruct() throws TranslationException {
+    throw new TranslationException();
+  }
+
+  @Test(expected = TranslationException.class)
+  public void canConstructWithMessage() throws TranslationException {
+    throw new TranslationException("Message");
+  }
+
+  @Test(expected = TranslationException.class)
+  public void canConstructWithCause() throws TranslationException {
+    throw new TranslationException(new RuntimeException());
+  }
+
+  @Test(expected = TranslationException.class)
+  public void canConstructWithMessageAndCause() throws TranslationException {
+    throw new TranslationException("message", new RuntimeException());
+  }
+}
